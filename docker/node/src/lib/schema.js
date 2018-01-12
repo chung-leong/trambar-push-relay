9db377d86854f6c1b089089bd6232bcdcbfa8ff8
Chung Leong <chernyshevsky@hotmail.com>
2018-01-12 16:25:54 +0100

    Finished implementing basic functionalities.

--- docker/node/src/lib/schema.js
+++ docker/node/src/lib/schema.js
@@ -5,23 +5,23 @@ exports.tables = `
         id serial,
         ctime timestamp NOT NULL DEFAULT NOW(),
         atime timestamp NOT NULL DEFAULT NOW(),
-        domain varchar(256) NOT NULL,
+        address varchar(256) NOT NULL,
         message_count bigint NOT NULL DEFAULT 0,
         PRIMARY KEY (id)
     );
-    CREATE UNIQUE INDEX ON ${name}.site (domain);
+    CREATE UNIQUE INDEX ON ${name}.site (address);
 
     CREATE TABLE ${name}.device (
         id serial,
         ctime timestamp NOT NULL DEFAULT NOW(),
         atime timestamp NOT NULL DEFAULT NOW(),
-        registration_id varchar(1024) NOT NULL,
         network varchar(32) NOT NULL,
-        endpoint_arn varchar(2048) NOT NULL,
+        registration_id varchar(1024) NOT NULL,
+        endpoint_arn varchar(2048),
         details jsonb NOT NULL DEFAULT '{}',
         message_count bigint NOT NULL DEFAULT 0,
-        current_domain varchar(256) NOT NULL,
-        current_token varchar(40) NOT NULL,
+        current_address varchar(256),
+        current_token varchar(64),
         PRIMARY KEY (id)
     );
     CREATE UNIQUE INDEX ON ${name}.device (registration_id);
--- docker/node/src/push-relay.js
+++ docker/node/src/push-relay.js
@@ -7,10 +7,12 @@ var BodyParser = require('body-parser');
 var DNSCache = require('dnscache');
 var Request = require('request');
 var Moment = require('moment');
+var Crypto = Promise.promisifyAll(require('crypto'));
+var AWS = require('aws-sdk');
+var CORS = require('cors');
 var Database = require('database');
 var Schema = require('schema');
 var HttpError = require('http-error');
-var AWS = require('aws-sdk');
 
 DNSCache({ enable: true, ttl: 300, cachesize: 1000 });
 
@@ -19,32 +21,48 @@ var FIFTEEN_MINUTE_RATE_LIMIT = parseInt(process.env.RATE_LIMIT) || 50000;
 var SSL_PRIVATE_KEY_PATH = process.env.SSL_PRIVATE_KEY_PATH;
 var SSL_CERTIFICATE_PATH = process.env.SSL_CERTIFICATE_PATH;
 
-var GCM_ARN = process.env.GCM_ARN;
+var FCM_ARN = process.env.FCM_ARN;
 var APNS_ARN = process.env.APNS_ARN;
 var WNS_ARN = process.env.WNS_ARN;
 
 var SNS = Promise.promisifyAll(new AWS.SNS({ apiVersion: '2010-03-31' }));
 var server;
 
+/**
+ * Start up service
+ *
+ * @return {Promise}
+ */
 function start() {
-    var app = Express();
-    app.use(BodyParser.json());
-    app.set('json spaces', 2);
-    app.post('/register', handleRegistration);
-    app.post('/dispatch', handleDispatch);
+    return Promise.try(() => {
+        var app = Express();
+        app.use(BodyParser.json());
+        app.use(CORS({
+            optionsSuccessStatus: 200,
+            methods: [ 'POST' ],
+        }));
+        app.set('json spaces', 2);
+        app.post('/register', handleRegistration);
+        app.post('/dispatch', handleDispatch);
 
-    if (SSL_PRIVATE_KEY_PATH && SSL_CERTIFICATE_PATH) {
-        var credentials = {
-            key: FS.readFileSync(SSL_PRIVATE_KEY_PATH),
-            cert: FS.readFileSync(SSL_CERTIFICATE_PATH),
-        };
-        server = HTTPS.createServer(credentials, app);
-    } else {
-        server = app.listen(80);
-    }
-    createSchema();
+        if (SSL_PRIVATE_KEY_PATH && SSL_CERTIFICATE_PATH) {
+            var credentials = {
+                key: FS.readFileSync(SSL_PRIVATE_KEY_PATH),
+                cert: FS.readFileSync(SSL_CERTIFICATE_PATH),
+            };
+            server = HTTPS.createServer(credentials, app);
+        } else {
+            server = app.listen(80);
+        }
+        createSchema();
+    });
 }
 
+/**
+ * Shut down service
+ *
+ * @return {Promise}
+ */
 function stop() {
     return new Promise((resolve, reject) => {
         if (server) {
@@ -58,8 +76,13 @@ function stop() {
     });
 };
 
+/**
+ * Create database schema
+ *
+ * @return {Promise}
+ */
 function createSchema() {
-    Promise.try(() => {
+    return Promise.try(() => {
         var db = Database.get();
         return db.schemaExists(Schema.name).then((exists) => {
             if (!exists) {
@@ -76,48 +99,103 @@ function createSchema() {
         if (err.code !== 'ECONNREFUSED') {
             console.error(err);
         }
-        setTimeout(createSchema, 5000);
+        return Promise.delay(5000).then(() => {
+            return createSchema();
+        });
     });
 }
 
+/**
+ * Send response to browser as JSON object
+ *
+ * @param  {Response} res
+ * @param  {Object} result
+ */
+function sendResponse(res, result) {
+    res.json(result);
+}
+
+/**
+ * Send error to browser as JSON object
+ *
+ * @param  {Response} res
+ * @param  {Object} err
+ */
+function sendError(res, err) {
+    var statusCode = err.statusCode;
+    var message = err.message;
+    if (!statusCode) {
+        // not an expected error
+        console.error(err);
+        statusCode = 500;
+        if (process.env.NODE_ENV === 'production') {
+            message = 'Internal server error';
+        }
+    }
+    res.status(statusCode).json({ message });
+}
+
+/**
+ * Handle registration request
+ *
+ * @param  {Request} req
+ * @param  {Response} res
+ *
+ * @return {Promise}
+ */
 function handleRegistration(req, res) {
     return Promise.try(() => {
+        var network = _.toLower(req.body.network);
         var registrationId = req.body.registration_id;
         var details = req.body.details || {};
-        var domain = req.body.domain;
-        var token = req.body.token;
+        var address = req.body.address || null;
         if (!registrationId) {
             throw new HttpError(400);
         }
-        return updateRegistration(registrationId, network, details, domain, token).then((row) => {
-            return {
-                ctime: Moment(row.ctime).toISOString(),
-                atime: Moment(row.atime).toISOString(),
-                message_count: row.message_count,
-            };
+        if (!_.includes([ 'fcm', 'apns', 'wns' ], network)) {
+            throw new HttpError(400);
+        }
+        return Crypto.randomBytesAsync(16).then((buffer) => {
+            var token = buffer.toString('hex');
+            return updateRegistration(network, registrationId, details, address, token).then((row) => {
+                return {
+                    token: row.current_token,
+                    ctime: Moment(row.ctime).toISOString(),
+                    atime: Moment(row.atime).toISOString(),
+                    message_count: row.message_count,
+                };
+            });
         });
     }).then((results) => {
-        sendJSON(results);
+        sendResponse(res, results);
     }).catch((err) => {
-        sendError(err);
+        sendError(res, err);
     });
 }
 
+/**
+ * Handle message dispatch request
+ *
+ * @param  {Request} req
+ * @param  {Response} res
+ *
+ * @return {Promise}
+ */
 function handleDispatch(req, res) {
     return Promise.try(() => {
         var signature = req.body.signature;
-        var domain = req.body.domain;
+        var address = req.body.address;
         var messages = req.body.messages;
-        if (!signature || !domain || !messages) {
+        if (!signature || !address || !messages) {
             throw new HttpError(400);
         }
-        return confirmSignature(domain, signature).then(() => {
-            return checkRateLimit(domain, messages.length).then(() => {
-                var recipientTokens = _.flatten(_.map(messages, 'tokens'));
+        return confirmSignature(address, signature).then(() => {
+            return checkRateLimit(address, messages.length).then(() => {
+                var recipientTokens = _.uniq(_.flatten(_.map(messages, 'tokens')));
                 var validTokens = [];
                 var deviceMessageCounts = {};
                 var errors = [];
-                return findListeningDevices(domain, tokens).each((device) => {
+                return findListeningDevices(address, recipientTokens).each((device) => {
                     var deviceMessages = _.filter(messages, (message) => {
                         return _.includes(message.tokens, device.current_token);
                     });
@@ -128,7 +206,7 @@ function handleDispatch(req, res) {
                         errors.push(err.message);
                     });
                 }).then(() => {
-                    return updateStatistics(domain, deviceMessageCounts);
+                    return updateStatistics(address, deviceMessageCounts);
                 }).then(() => {
                     var invalidTokens = _.difference(recipientTokens, validTokens);
                     return {
@@ -139,47 +217,50 @@ function handleDispatch(req, res) {
             });
         });
     }).then((results) => {
-        sendJSON(results);
+        sendResponse(res, results);
     }).catch((err) => {
-        sendError(err);
+        sendError(res, err);
     });
 }
 
 /**
- * Set current_domain and current_token of device record
+ * Set current_address and current_token of device record
  *
+ * @param  {String} network
  * @param  {String} registrationId
- * @param  {String} domain
+ * @param  {String} details
+ * @param  {String} address
  * @param  {String} token
  *
  * @return {Promise<Object|undefined>}
  */
-function updateRegistration(registrationId, network, details, domain, token) {
-    var db = Databse.get();
+function updateRegistration(network, registrationId, details, address, token) {
+    var db = Database.get();
     var sql = `
         UPDATE ${Schema.name}.device
-        SET current_domain = $1, current_token = $2
-        WHERE registration_id = $3 AND network = $4
+        SET current_address = $1, current_token = $2, atime = NOW()
+        WHERE network = $3 AND registration_id = $4
         RETURNING *;
     `;
-    var params = [ domain, token, registrationId, network ];
+    var params = [ address, token, network, registrationId ];
     return db.query(sql, params).then((rows) => {
         if (rows.length > 0) {
             return rows[0];
         }
         var sql = `
             INSERT INTO ${Schema.name}.device (
-                registration_id,
                 network,
+                registration_id,
                 details,
-                current_domain,
+                current_address,
                 current_token
             )
             VALUES ($1, $2, $3, $4, $5)
-            ON CONFLICT DO UPDATE SET current_domain = $4, current_token = $5
+            ON CONFLICT (registration_id)
+            DO UPDATE SET current_address = $4, current_token = $5, atime = NOW()
             RETURNING *;
         `;
-        var params = [ domain, token, details, registrationId, network ];
+        var params = [ network, registrationId, details, address, token ];
         return db.query(sql, params).then((rows) => {
             return rows[0];
         });
@@ -187,54 +268,56 @@ function updateRegistration(registrationId, network, details, domain, token) {
 }
 
 /**
- * Find rows in device when current domain and token match the ones given
+ * Find rows in device when current address and token match the ones given
  *
- * @param  {String} domain
+ * @param  {String} address
  * @param  {String} tokens
  *
  * @return {Promise<Array<Object>>}
  */
-function findListeningDevices(domain, tokens) {
-    var db = Databse.get();
+function findListeningDevices(address, tokens) {
+    var db = Database.get();
     var sql = `
         SELECT * FROM ${Schema.name}.device
-        WHERE current_domain = $1 AND current_token = ANY($2)
+        WHERE current_address = $1 AND current_token = ANY($2)
     `;
-    var params = [ domain, tokens ];
-    return db.query(sql, params).get(0);
+    var params = [ address, tokens ];
+    return db.query(sql, params);
 }
 
 /**
  * Increment message_count in site and device
  *
- * @param  {String} domain
+ * @param  {String} address
  * @param  {Object<Number>} messageCounts
  *
  * @return {Promise}
  */
-function updateStatistics(domain, messageCounts) {
+function updateStatistics(address, messageCounts) {
     // messageCounts is keyed by device ids
     var totalMessageCount = _.sum(_.values(messageCounts));
     if (totalMessageCount === 0) {
         return Promise.resolve();
     }
+    var db = Database.get();
     var sql = `
         UPDATE ${Schema.name}.site
         SET message_count = message_count + $1
-        WHERE domain = $2
+        WHERE address = $2
     `;
-    var params = [ totalMessageCount, domain ];
+    var params = [ totalMessageCount, address ];
     return db.execute(sql, params).then((result) => {
         if (result.rowCount === 0) {
             var sql = `
                 INSERT INTO ${Schema.name}.site (
-                    domain,
+                    address,
                     message_count
                 )
                 VALUES ($1, $2)
-                ON CONFLICT DO UPDATE SET message_count = message_count + $2
+                ON CONFLICT (address)
+                DO UPDATE SET message_count = ${Schema.name}.site.message_count + $2
             `;
-            var params = [ domain, totalMessageCount ];
+            var params = [ address, totalMessageCount ];
             return db.execute(sql, params);
         }
     }).then(() => {
@@ -253,27 +336,32 @@ function updateStatistics(domain, messageCounts) {
     });
 }
 
+function confirmSignature(address, signature) {
+    // TODO
+    return Promise.resolve();
+}
+
 var messageCountsByDomain = {};
 var previousPeriod = getTimeRoundedTo15Min();
 
 /**
  * Reject with HTTP status code 429 if too many messages have been sent from
- * the given domain already
+ * the given address already
  *
- * @param  {String} domain
+ * @param  {String} address
  * @param  {Number} additional
  *
  * @return {Promise}
  */
-function checkRateLimit(domain, additional) {
+function checkRateLimit(address, additional) {
     var currentPeriod = getTimeRoundedTo15Min();
     if (previousPeriod !== currentPeriod) {
         messageCountsByDomain = {};
         currentPeriod = previousPeriod;
     }
-    var currentCount = messageCountsByDomain[domain] || 0;
+    var currentCount = messageCountsByDomain[address] || 0;
     if (currentCount + additional < FIFTEEN_MINUTE_RATE_LIMIT) {
-        messageCountsByDomain[domain] = currentCount + additional;
+        messageCountsByDomain[address] = currentCount + additional;
         return Promise.resolve();
     } else {
         return Promise.reject(new HttpError(429));
@@ -317,10 +405,22 @@ function sendMessages(device, messages) {
  */
 function sendMessage(device, message) {
     return createEndPoint(device).then((endPointARN) => {
+        var platformMessageBody = message[device.network];
+        var messageBody = {};
+        var protocol;
+        switch (device.network) {
+            case 'fcm': protocol = 'GCM'; break;
+            case 'apns': protocol = 'APNS'; break;
+            case 'wns': protocol = 'WNS'; break;
+        }
+        messageBody[protocol] = JSON.stringify(platformMessageBody);
+        console.log(platformMessageBody);
+        console.log(messageBody, device);
+        var messageJSON = JSON.stringify(messageBody);
         var params = {
-          Message: _.pickBy(message, _.toUpper(device.network)),
-          MessageStructure: 'json',
-          TargetArn: device.endpoint_arn,
+            Message: messageJSON,
+            MessageStructure: 'json',
+            TargetArn: device.endpoint_arn,
         };
         return SNS.publishAsync(params).then((data) => {
             return data.MessageId;
@@ -341,8 +441,8 @@ function createEndPoint(device) {
     }
     var applicationARN;
     switch (device.network) {
-        case 'gcm':
-            applicationARN = GCM_ARN;
+        case 'fcm':
+            applicationARN = FCM_ARN;
             break;
         case 'apns':
             applicationARN = APNS_ARN;
@@ -363,6 +463,28 @@ function createEndPoint(device) {
     });
 }
 
+/**
+ * Update a device's SNS endpoint
+ *
+ * @param  {Object} device
+ * @param  {endpointARN} endPointARN
+ *
+ * @return {Object}
+ */
+function updateDeviceEndpoint(device, endPointARN) {
+    var db = Database.get();
+    var sql = `
+        UPDATE ${Schema.name}.device
+        SET endpoint_arn = $1
+        WHERE id = $2
+        RETURNING *;
+    `;
+    var params = [ endPointARN, device.id ];
+    return db.query(sql, params).then((rows) => {
+        return rows[0];
+    });
+}
+
 exports.start = start;
 exports.stop = stop;
 
@@ -377,7 +499,6 @@ _.each(['SIGTERM', 'SIGUSR2'], (sig) => {
         });
     });
 });
-
 
 process.on('uncaughtException', function(err) {
     console.error(err);
